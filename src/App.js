5697ff7cfb9f77e42747b23d6c43887cffec8c4f
hir0112 <hiroki.k_12@icloud.com>
2025-12-09 04:38:06 +0900

    整理

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { DashboardPage } from './components/DashboardPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
